d7b510ac6ea24532641694c494bdf70abf6807e8
stormxing <stormxing@foxmail.com>
2018-01-28 13:36:58 +0800

    完成惊悚片信息录入

--- theater/js/database/thriller.js
+++ theater/js/database/thriller.js
@@ -1,4 +1,120 @@
 // 惊悚/悬疑片
 var thriller1= [
-    {}
+    {
+        id: 1291992,
+        alt: "https://movie.douban.com/subject/1291992/",
+        year: 1991,
+        title: "末路狂花",
+        rating: 8.7,
+        original_title: "Thelma & Louise",
+        directors: "雷德利·斯科特",
+        casts: "吉娜·戴维斯,苏珊·萨兰登,哈威·凯特尔",
+        genres: "犯罪,剧情,惊悚",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p794583044.jpg"
+    }
+];
+
+var thriller2= [
+    {
+        id: 1292217,
+        alt: "https://movie.douban.com/subject/1292217/",
+        year: 2001,
+        title: "穆赫兰道",
+        rating: 8.3,
+        original_title: "Mulholland Dr.",
+        directors: "大卫·林奇",
+        casts: "娜奥米·沃茨,劳拉·哈灵,安·米勒",
+        genres: "剧情,悬疑,惊悚",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p792248233.jpg"
+    }
+];
+
+var thriller3= [
+    {
+        id: 1293544,
+        alt: "https://movie.douban.com/subject/1293544/",
+        year: 1991,
+        title: "沉默的羔羊",
+        rating: 8.7,
+        original_title: "The Silence of the Lambs",
+        directors: "乔纳森·戴米",
+        casts: "朱迪·福斯特,安东尼·霍普金斯,斯科特·格伦",
+        genres: "剧情,犯罪,惊悚",
+        image: "https://img1.doubanio.com/view/movie_poster_cover/spst/public/p1593414327.jpg"
+    }
+];
+
+var thriller4= [
+    {
+        id: 1297192,
+        alt: "https://movie.douban.com/subject/1297192/",
+        year: 2003,
+        title: "致命ID",
+        rating: 8.6,
+        original_title: "Identity",
+        directors: "詹姆斯·曼高德",
+        casts: "约翰·库萨克,雷·利奥塔,阿曼达·皮特",
+        genres: "剧情,悬疑,惊悚",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p453720880.jpg"
+    }
+];
+
+var thriller5= [
+    {
+        id: 1299327,
+        alt: "https://movie.douban.com/subject/1299327/",
+        year: 1994,
+        title: "夜访吸血鬼",
+        rating: 8.3,
+        original_title: "Interview with the Vampire: The Vampire Chronicles",
+        directors: "尼尔·乔丹",
+        casts: "布拉德·皮特,汤姆·克鲁斯,克斯汀·邓斯特",
+        genres: "剧情,奇幻,惊悚",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1353910572.jpg"
+    }
+];
+
+var thriller6= [
+    {
+        id: 1304102,
+        alt: "https://movie.douban.com/subject/1304102/",
+        year: 2002,
+        title: "谍影重重",
+        rating: 8.5,
+        original_title: "The Bourne Identity",
+        directors: "道格·里曼",
+        casts: "马特·达蒙,弗朗卡·波滕特,克里斯·库珀",
+        genres: "动作,悬疑,惊悚",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1597183981.jpg"
+    }
+];
+
+var thriller7= [
+    {
+        id: 1308767,
+        alt: "https://movie.douban.com/subject/1308767/",
+        year: 2004,
+        title: "谍影重重2",
+        rating: 8.5,
+        original_title: "The Bourne Supremacy",
+        directors: "保罗·格林格拉斯",
+        casts: "马特·达蒙,弗朗卡·波滕特,布莱恩·考克斯",
+        genres: "动作,悬疑,惊悚",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p667644866.jpg"
+    }
+];
+
+var thriller8= [
+    {
+        id: 1309027,
+        alt: "https://movie.douban.com/subject/1309027/",
+        year: 2005,
+        title: "罪恶之城",
+        rating: 8.4,
+        original_title: "Sin City",
+        directors: "弗兰克·米勒,罗伯特·罗德里格兹,昆汀·塔伦蒂诺",
+        casts: "杰西卡·阿尔芭,布鲁斯·威利斯,克里夫·欧文",
+        genres: "动作,犯罪,惊悚",
+        image: "https://img1.doubanio.com/view/movie_poster_cover/spst/public/p2173716829.jpg"
+    }
 ];
