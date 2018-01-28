eb33f8341bfb30d4c801260b6797c4baf046a013
stormxing <stormxing@foxmail.com>
2018-01-28 13:40:07 +0800

    完成最新推荐信息录入

--- theater/js/database/new.js
+++ theater/js/database/new.js
@@ -13,3 +13,109 @@ var new1= [
         image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2070153774.jpg"
     }
 ];
+
+var new2 = [
+    {
+        id: 1291545,
+        alt: "https://movie.douban.com/subject/1291545/",
+        year: 2003,
+        title: "大鱼",
+        rating: 8.7,
+        original_title: "Big Fish",
+        directors: "蒂姆·波顿",
+        casts: "伊万·麦克格雷格,阿尔伯特·芬尼,比利·克鲁德普",
+        genres: "剧情,家庭,奇幻",
+        image: "https://img3.doubanio.com/view/mo " +
+        " vie_poster_cover/spst/public/p692813374.jpg"
+    }
+];
+
+var new3= [
+    {
+        id: 1308767,
+        alt: "https://movie.douban.com/subject/1308767/",
+        year: 2004,
+        title: "谍影重重2",
+        rating: 8.5,
+        original_title: "The Bourne Supremacy",
+        directors: "保罗·格林格拉斯",
+        casts: "马特·达蒙,弗朗卡·波滕特,布莱恩·考克斯",
+        genres: "动作,悬疑,惊悚",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p667644866.jpg"
+    }
+];
+
+var new4 = [
+    {
+        id: 1295038,
+        alt: "https://movie.douban.com/subject/1295038/",
+        year: 2001,
+        title: "哈利·波特与魔法石",
+        rating: 8.6,
+        original_title: "Harry Potter and the Sorcerer's Stonee",
+        directors: "克里斯·哥伦布",
+        casts: "丹尼尔·雷德克里夫,艾玛·沃森,鲁伯特·格林特",
+        genres: "奇幻,冒险",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p804947166.jpg"
+    }
+];
+
+var new5 = [
+    {
+        id: 1298070,
+        alt: "https://movie.douban.com/subject/1298070/",
+        year: 2003,
+        title: "加勒比海盗",
+        rating: 8.6,
+        original_title: "Pirates of the Caribbean: The Curse of the Black Pearl",
+        directors: "戈尔·维宾斯基",
+        casts: "约翰尼·德普,杰弗里·拉什,奥兰多·布鲁姆",
+        genres: "动作,冒险,奇幻",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1596085504.jpg"
+    }
+];
+
+var new6= [
+    {
+        id: 1297192,
+        alt: "https://movie.douban.com/subject/1297192/",
+        year: 2003,
+        title: "致命ID",
+        rating: 8.6,
+        original_title: "Identity",
+        directors: "詹姆斯·曼高德",
+        casts: "约翰·库萨克,雷·利奥塔,阿曼达·皮特",
+        genres: "剧情,悬疑,惊悚",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p453720880.jpg"
+    }
+];
+
+var new7 = [
+    {
+        id: 1291990,
+        alt: "https://movie.douban.com/subject/1291990/",
+        year: 2004,
+        title: "爱在日落黄昏时",
+        rating: 8.7,
+        original_title: "Before Sunset",
+        directors: "理查德·林克莱特",
+        casts: "伊桑·霍克,朱莉·德尔佩,弗农·多布切夫",
+        genres: "剧情,爱情",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1910924055.jpg"
+    }
+];
+
+var new8 = [
+    {
+        id: 1291999,
+        alt: "https://movie.douban.com/subject/1291999/",
+        year: 1994,
+        title: "重庆森林",
+        rating: 8.6,
+        original_title: "重慶森林",
+        directors: "王家卫",
+        casts: "林青霞,金城武,梁朝伟",
+        genres: "剧情,爱情",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p792381411.jpg"
+    }
+];
