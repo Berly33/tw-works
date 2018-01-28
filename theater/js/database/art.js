5c1afa0204b85619062e2fde432786eb134dc94d
lucky5678 <1712477640@qq.com>
2018-01-28 14:19:43 +0800

    完成艺术片的数据录入

--- theater/js/database/art.js
+++ theater/js/database/art.js
@@ -1,4 +1,116 @@
 // 艺术片
 var art1= [
-    {}
+    {
+        id:1291549,
+        alt:"https://movie.douban.com/subject/1291549/",
+        year:2004,
+        title:"放牛班的春天",
+        rating:9.2,
+        origin_title:"Les choristes",
+        directors: "克里斯托夫·巴拉蒂",
+        casts: "热拉尔·朱尼奥,让-巴蒂斯特·莫尼耶,弗朗索瓦·贝莱昂",
+        genres: "剧情,音乐",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1910824951.jpg"
+    }
+];
+var art2=[
+    {
+        id:1292001,
+        alt:"https://movie.douban.com/subject/1292001/",
+        year:1998,
+        title:"海上钢琴师",
+        rating:9.2,
+        origin_title:"La leggenda del pianista sull'oceano",
+        directors: "朱塞佩·托纳多雷",
+        casts: "蒂姆·罗斯,普路特·泰勒·文斯,比尔·努恩",
+        genres: "剧情,音乐",
+        image: "https://img1.doubanio.com/view/movie_poster_cover/spst/public/p511146957.jpg"
+    }
+];
+var art3=[
+    {
+
+        id:1292218,
+        alt:"https://movie.douban.com/subject/1292218/",
+        year:1998,
+        title:"中央车站",
+        rating:8.7,
+        origin_title:"Central do Brasil",
+        directors: "沃尔特·塞勒斯",
+        casts: "费尔兰德·蒙特纳哥,马里利娅·佩拉,文尼西斯·狄·奥利维拉",
+        genres: "剧情,音乐",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2239441575.jpg"
+    }
+];
+var art4=[
+    {
+
+        id:1292224,
+        alt:"https://movie.douban.com/subject/1292224/",
+        year:1975,
+        title:"飞越疯人院",
+        rating:9,
+        origin_title:"One Flew Over the Cuckoo's Nest",
+        directors: "米洛斯·福尔曼",
+        casts: "杰克·尼科尔森,丹尼·德维托,克里斯托弗·洛伊德",
+        genres: "剧情,音乐",
+        image: "https://img1.doubanio.com/view/movie_poster_cover/spst/public/p792238287.jpg"
+}
+];
+var art5=[
+    {
+
+        id:1292262,
+        alt:"https://movie.douban.com/subject/1292262/",
+        year:1984,
+        title:"美国往事",
+        rating:9.1,
+        origin_title:"Once Upon a Time in America",
+        directors: "赛尔乔·莱翁内",
+        casts: "罗伯特·德尼罗,詹姆斯·伍兹,伊丽莎白·麦戈文",
+        genres: "剧情,音乐",
+        image: "https://img1.doubanio.com/view/movie_poster_cover/spst/public/p477229647.jpg"
+    }
+];
+var art6=[
+    {
+        id:1787291,
+        alt:"https://movie.douban.com/subject/1292262/",
+        year:2006,
+        title:"被嫌弃的松子的一生",
+        rating:8.9,
+        origin_title:"嫌われ松子の一生",
+        directors: "中岛哲也	",
+        casts: "中谷美纪,瑛太,香川照之",
+        genres: "剧情,歌舞",
+        image: "https://img1.doubanio.com/view/movie_poster_cover/spst/public/p453723669.jpg"
+    }
+];
+var art7=[
+    {
+        id:2053515,
+        alt:"https://movie.douban.com/subject/2053515/",
+        year:2006,
+        title:"曾经",
+        rating:8.3,
+        origin_title:"Once",
+        directors: "约翰·卡尼	",
+        casts: "格伦·汉塞德,玛可塔·伊尔格洛娃,Alaistair Foley",
+        genres: "剧情,音乐,爱情	",
+        image: "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2173720203.jpg"
+    }
+];
+var art8=[
+    {
+        id:2149806,
+        alt:"https://movie.douban.com/subject/2149806/",
+        year:2008,
+        title:"入殓师",
+        rating:8.8,
+        origin_title:"おくりびと",
+        directors: "泷田洋二郎",
+        casts: "本木雅弘,广末凉子,山崎努",
+        genres: "剧情",
+        image: "https://img1.doubanio.com/view/movie_poster_cover/spst/public/p594972928.jpg"
+    }
 ];
